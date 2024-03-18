76a8edafaa45bf84e9be24fbc06b279d259bc3e6
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-03-18 17:52:31 +0530

    connected application to the database

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# with app.app_context():
+#     db.create_all()
+ 
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return "hello world!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+     
